db65d73a2fe6c77511bce5f0e8202b159c0bda0b
guofeng <guofengzh@gmail.com>
2018-07-24 09:21:19 +0800

    initial commit

--- src/main/java/com/mtoliv/iot/server/api/TcpReader.java
+++ src/main/java/com/mtoliv/iot/server/api/TcpReader.java
@@ -9,7 +9,7 @@ public interface TcpReader {
      *  消息已被解码，调用此方法
      *
      * @param session
-     * @param msg
+     * @param msg 已被解码的消息
      * @return  返回要被发送的响应
      */
     public Optional<Object> readerCallback(Session session, Object msg) ;
