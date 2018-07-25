e9a3cac3e4a5f98a1d379c319f431a74fb09ce16
guofeng zhang <guofeng.zhang@mybean.cn>
2018-07-26 01:23:21 +0800

    payload definition

--- src/main/java/com/mtoliv/iot/server/codec/ResponseMessageEncoder.java
+++ src/main/java/com/mtoliv/iot/server/codec/ResponseMessageEncoder.java
@@ -1,15 +1,16 @@
 package com.mtoliv.iot.server.codec;
 
+import com.mtoliv.iot.server.message.GBT26875Message;
 import com.mtoliv.iot.server.message.ResponseMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-public class ResponseMessageEncoder extends MessageToByteEncoder<ResponseMessage> {
+public class ResponseMessageEncoder extends MessageToByteEncoder<GBT26875Message> {
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, ResponseMessage msg, ByteBuf out) throws Exception {
+    protected void encode(ChannelHandlerContext ctx, GBT26875Message msg, ByteBuf out) throws Exception {
         // 将msg按线上协议编码,发送到线上
-        out.writeInt(msg.getIntValue());
+        msg.toByteBuffer(out);
     }
 }
