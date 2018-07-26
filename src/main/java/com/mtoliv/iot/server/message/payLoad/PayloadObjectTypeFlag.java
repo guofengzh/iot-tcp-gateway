21026e17bc5e925dcef1fa174c3b21911635007b
guofeng <guofengzh@gmail.com>
2018-07-26 12:41:03 +0800

    fix package name issue

--- src/main/java/com/mtoliv/iot/server/message/payLoad/PayloadObjectTypeFlag.java
+++
@@ -1,9 +0,0 @@
-package com.mtoliv.iot.server.message.payLoad;
-
-public interface PayloadObjectTypeFlag {
-    // 上行
-    byte FC01XiTongZhuangTai = (byte)1 ;
-
-    // 下行
-    byte FC90XingXiChuanFuZhuangZhiShiJiang = (byte)90 ;
-}
